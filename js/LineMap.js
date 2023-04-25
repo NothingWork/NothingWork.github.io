0f046cf538b6e87c60f351d5b75fceae83c2ec92
王祺骜 <1175402300@qq.com>
2023-04-25 19:46:40 +0800

    add files

---
+++ js/LineMap.js
@@ -0,0 +1,68 @@
+var year = []
+var data = []
+$.ajax({
+    url: './dataset/折线图DATA.csv',
+    type: 'GET',
+    async: true,
+    success: function (testData) {
+        var m = $.csv.toArrays(testData, {
+            delimiter: ","
+        })
+        year = m[0]
+        data = m[6]
+        // console.log(m);
+        var lineMap = echarts.init(document.getElementById('LineMap'));
+
+        var optionForLineMap = {
+            tooltip: {
+                trigger: 'axis',
+                formatter: 'Emission : <br/>{b}年 : {c}Mt'
+            },
+            grid: {
+                left: '1%',
+                right: '6%',
+                bottom: '3%',
+                containLabel: true
+            },
+            xAxis: {
+                type: 'value',
+                axisLabel: {
+                    formatter: function (value, index) {
+                        if (value >= 10000 && value < 10000000) {
+                            value = value / 10000 + "万";
+                        } else if (value >= 10000000) {
+                            value = value / 10000000 + "千万";
+                        }
+                        return value;
+                    }
+                }
+            },
+            yAxis: {
+                type: 'category',
+                axisLine: { onZero: false },
+                axisLabel: {
+                    formatter: '{value}年'
+                },
+                boundaryGap: false,
+                data: year
+            },
+            series: [
+                {
+                    name: 'Altitude (km) vs. Emission (°C)',
+                    type: 'line',
+                    symbolSize: 10,
+                    symbol: 'circle',
+                    smooth: true,
+                    lineStyle: {
+                        width: 3,
+                        shadowColor: 'rgba(0,0,0,0.3)',
+                        shadowBlur: 10,
+                        shadowOffsetY: 8
+                    },
+                    data: data
+                }
+            ]
+        };
+        lineMap.setOption(optionForLineMap);
+    }
+});
